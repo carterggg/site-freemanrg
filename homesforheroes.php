6814ed4650a111b5ddb600576a0f402fae2a8975
Carter G <carterguthrie94@yahoo.com>
2015-10-11 17:59:03 -0400

    updated

--- homesforheroes.php
+++ homesforheroes.php
@@ -125,26 +125,37 @@
 
         <div class="row">
             <div class="box">
-                <div class="col-lg-12">
+                <div class="col-xs-12 col-lg-12">
 
                     <h2 class="intro-text text-center">
                         <strong>Homes For Heroes</strong>
                     </h2>
                     <hr>
                 </div>
-                <div class="col-md-6">
+                <div class="col-xs-12 col-md-6">
                   <br>
                    <h2 class="intro-text"><strong>What is Home For Heroes?</strong></h2>
              
                     <p>The Nation's Largest Hero Savings Program COR Residential has partnered with Homes For Heroes, a national program that offers discounts for Firefighters, Emergency Medical Technicians, Military and Veterans, Law Enforcement, Teachers, Clergy, and Healthcare Workers when buying or selling a home.</p>
                     <p>When Heroes buy or sell a home using Michael as their agent they receive substantial savings!</p>
                 </div>
-                <div class="col-md-6">
-                    <img class="img-responsive hidden-xs align-center" src="images/layout/homesforheroes-tagline.jpg" alt="Freeman Realty Group" width="400px">
-                    
-                    <img class="img-responsive img-border-left hidden-sm hidden-md hidden-lg align-center" src="images/layout/heroes-logo2.jpg" alt="Freeman Realty Group">
+                <div class="col-xs-12 col-md-6">
+                    <img class="img-responsive hidden-xs hidden-sm align-center" src="images/layout/homesforheroes-tagline.jpg" alt="Freeman Realty Group" width="400px">
+
                 </div>
                 <div class="clearfix"></div>
+                		<div class="row hidden-md hidden-lg">
+                			<div class="col-xs-12 col-sm-6">
+                				<img class="img-responsive align-center" src="images/layout/homesforheroes-tagline.jpg" alt="Freeman Realty Group">
+                			</div>
+                			<div class="col-xs-12 col-sm-6">
+               					
+                					<br class="hidden-xs hidden-md hidden-lg"><br class="hidden-xs hidden-md hidden-lg">
+                				<img src="images/layout/grouppic.jpg" alt="Homes For Heroes" class="img-responsive align-center">
+                			</div>                			
+                		</div>
+                		
+                
             </div>
         </div>
         
@@ -152,9 +163,8 @@
         
 		 <div class="row">
             
-                <div class="col-md-6">
-                  <br>
-                 	<img src="images/layout/grouppic.jpg" alt="Homes For Heroes" class="img-responsive align-center" width="400px">
+                <div class="col-xs-12 col-md-6">
+                 	<img src="images/layout/grouppic.jpg" alt="Homes For Heroes" class="img-responsive align-center hidden-xs hidden-sm" width="400px">
                   
 					<h2 class="intro-text"><strong>Here Are The Savings We Offer Heroes Using The Homes For Heroes® Program:</strong></h2>
                     <ul>
@@ -167,10 +177,10 @@
                     	
 
                 </div>
-                <div class="col-md-6">
+                <div class=" col-xs-12 col-md-6">
                     <h2 class="intro-text"><strong>Register As A Hero:</strong></h2>
                     	<?php include('inc/form2.php'); ?>
-                    	
+                    	<br><br>
                 </div>
                 <div class="clearfix"></div>
             
